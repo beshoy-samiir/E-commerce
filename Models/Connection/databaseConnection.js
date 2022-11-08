fbb1b01a00f42c3198276d16ad0446b5f058a636
andrew2237 <andrew.saed5858@gmail.com>
2022-11-08 02:19:09 +0200

    make it better

--- Models/Connection/databaseConnection.js
+++ Models/Connection/databaseConnection.js
@@ -1,4 +1,6 @@
 const mongoose=require("mongoose")
 // mongoose.connect("mongodb+srv://donya:6rtz6QZajJSlbdqk@cluster0.kpnsh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-mongoose.connect("mongodb+srv://andrew01:Y41lKQEoV2MuqNOc@cluster0.hgr8wks.mongodb.net/?retryWrites=true&w=majority")
+mongoose.connect("mongodb+srv://andrew22:C72ex4trkV9PYfbw@cluster0.hgr8wks.mongodb.net/?retryWrites=true&w=majority")
 // mongodb+srv://andrew01:Y41lKQEoV2MuqNOc@cluster0.hgr8wks.mongodb.net/?retryWrites=true&w=majority
+// C72ex4trkV9PYfbw
+// andrew22
